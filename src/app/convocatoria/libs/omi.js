6a2dd67b05246b8e2b91e310677d302963ba70ed
OMIChihuahua <166193770+OMIChihuahua@users.noreply.github.com>
2024-04-09 18:05:15 -0600

    Update omi.js

--- src/app/convocatoria/libs/omi.js
+++ src/app/convocatoria/libs/omi.js
@@ -1,11 +1,11 @@
 const convocatoriaOMI = `
 # **Olimpiada Mexicana de Informática Chihuahua**
 
-El Gobierno de Chihuahua a través de la Secretaría de Educación y Deporte y la Olimpiada Mexicana de Informática Chihuahua (OMICHH).
+La Olimpiada Mexicana de Informática Chihuahua (OMICHH).
 
-## **CONVOCAN**
+## **CONVOCA**
 
-A todos los alumnos que radiquen en el Estado de Chihuahua a participar en la 28° Olimpiada Mexicana de Informática en su fase Estatal, la cual permitirá seleccionar a los alumnos más destacados para su participación en la OMI Nacional 2023 a celebrarse la primera quincena del mes de Octubre de 2023.
+A todos los alumnos que radiquen en el Estado de Chihuahua a participar en la 29° Olimpiada Mexicana de Informática en su fase Estatal, la cual permitirá seleccionar a los alumnos más destacados para su participación en la OMI Nacional 2023 a celebrarse la primera quincena del mes de Octubre de 2023.
 
 La **Olimpiada Mexicana de Informática en Chihuahua (OMICHH)** es un concurso a nivel estatal para jóvenes y niños con facilidad para resolver problemas prácticos mediante la lógica y el uso de computadoras, que busca promover el desarrollo tecnológico en México y encontrar a los mejores programadores, quienes formarán la selección del estado para participar en las próximas Olimpiadas Nacionales de Informática (OMI) y posteriormente en Olimpiadas Internacionales de Informática (IOI).
 
@@ -20,19 +20,18 @@ Promover la amistad, convivencia e intercambio tecnológico entre jóvenes de to
 
 ### **I. Requisitos y Condiciones.**
 
-Haber nacido después del 1° de julio de 2004.
+Haber nacido después del 1° de julio de 2005.
 Tener nacionalidad mexicana o radicar en México y estar dispuesto a representar internacionalmente a México.
 Estar inscrito en una institución de nivel básico o medio superior con una
 carga curricular completa en el sistema educativo mexicano.
-Firmar de conformidad el reglamento y código de conducta por parte del alumno y padre o tutor, el cual será enviado una vez que se realice el correspondiente registro.
 Estar cursando un grado tal que para el semestre de enero a junio del 2024 aún se encuentre en nivel básico o medio superior.
-
+Leer y acatar el reglamento oficial publicado en la siguiente [liga](www.olimpiadadeinformatica.org.mx/OMI/OMI/InfoGeneral/Reglamento.aspx)
 
 Los conocimientos y habilidades mínimas con las que deberán contar son las siguientes: Matemáticas básicas: Aritmética, Álgebra y Trigonometría. Gusto y placer por resolver problemas y retos.
 
 ### **II.  Inscripciones**
-Las inscripciones iniciarán el 18 de febrero de 2023 y finalizarán el 31 de agosto de 2023.
-Los concursantes deben registrarse en la siguiente liga. www.omichh.org
+Las inscripciones iniciarán el 10 de abril de 2024 y finalizarán el 1 de agosto de 2024.
+Los concursantes deben registrarse a travez de sus maestros asesores o en su defecto el padre o tutor deberá enviar un correo a contacto@omichh.org.
 Asesores si desean recibir más información, favor de enviar un correo electrónico a contacto@omichh.org.
 
 Para más información visita nuestra página oficial www.omichh.org o cualquier duda envía un correo electrónico a contacto@omichh.org.
@@ -45,9 +44,8 @@ El proceso Estatal de la OMICHH se desarrolla en las etapas y fechas a continuac
 Categoría Abierta
 * Inscripciones.
 * Desarrollo del curso estatal en omegaup, al cual serán agregados una vez se registren.
-* Examen Pre nacional se llevará a cabo de manera presencial en la ciudad de Chihuahua, Chih. Durante el mes de septiembre de 2023.
-* El sábado 2 de septiembre de 2023 a las 10:00 hrs a 14:30 hrs. Se efectuará la primera fase del examen estatal de manera virtual.
-* El sábado 9 de septiembre de 2023 a las 10:00 hrs a 14:30 hrs. Se efectuará la segunda fase del examen estatal de manera presencial, la sede del examen se informará próximo a la fecha.
+* El sábado 31 de agosto de 2024 a las 9:00 hrs a 14:30 hrs. 5 problemas de manera virtual.
+* El sábado 7 de septiembre de 2024 a las 10:00 hrs a 14:30 hrs. Se efectuará el examen pre nacional de manera presencial, la sede del examen se informará próximo a la fecha.
 * El examen Pre-Nacional será el que determinará a los 4 alumnos que integraran a la delegación nacional que representará a Chihuahua en la OMI Nacional.
 
 
